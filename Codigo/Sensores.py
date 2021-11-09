9e6c2a24a7a9150954d95264ff2e00eee51fcf36
Ana Karen Martinez <71353396+Karen25-2@users.noreply.github.com>
2021-11-08 20:52:02 -0800

    Create Sensores.py

---
+++ Codigo/Sensores.py
@@ -0,0 +1,28 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+# Define los colores rojo y verde
+
+rojo = (255, 0, 0)
+verde = (0, 255, 0)
+
+# Toma lecturas de los tres sensores
+
+t = sense.get_temperature() # temperatura
+p = sense.get_pressure() # presión
+h = sense.get_humidity() # humedad
+
+# Redondea los valores a un lugar decimal
+t = round(t, 1)
+p = round(p, 1)
+h = round(h, 1)
+
+# str() convierte el valor en una cadena de caracteres, para que pueda ser concatenado
+
+message = "Temperatura: " + str(t) + " Presión: " + str(p) + "Humedad: " + str(h)
+if t > 18.3 and h > 50 and p> 800:
+   bg = verde
+else:
+ bg = rojo
+ 
+# Muestra el mensaje
+sense.show_message(message, scroll_speed=0.05, back_colour=bg) 
